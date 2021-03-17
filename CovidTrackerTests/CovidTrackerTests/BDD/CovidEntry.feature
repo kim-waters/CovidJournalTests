45bef0c0b4400ed4e548a3d0a801bfd24ce2b759
Kim Waters <kimwaters1996@gmail.com>
2021-03-17 14:06:32 +0000

    final unhappy path test for login

--- CovidTrackerTests/CovidTrackerTests/BDD/CovidEntry.feature
+++ CovidTrackerTests/CovidTrackerTests/BDD/CovidEntry.feature
@@ -1,9 +1,9 @@
 ﻿Feature: CovidEntry
 	Make daily entries on symptoms
-
-@navigate
-Scenario: Navigate to the entry page
-	Given the user is signed in
-	And the user is on the homepage
-	When the covid entry link is clicked
-	Then the user is directed to the covid entry page
+#
+#@navigate
+#Scenario: Navigate to the entry page
+#	Given the user is signed in
+#	And the user is on the homepage
+#	When the covid entry link is clicked
+#	Then the user is directed to the covid entry page
--- CovidTrackerTests/CovidTrackerTests/BDD/CovidEntry.feature.cs
+++ CovidTrackerTests/CovidTrackerTests/BDD/CovidEntry.feature.cs
@@ -72,51 +72,6 @@ namespace CovidTrackerTests.BDD
         {
             testRunner.CollectScenarioErrors();
         }
-        
-        [NUnit.Framework.TestAttribute()]
-        [NUnit.Framework.DescriptionAttribute("Navigate to the entry page")]
-        [NUnit.Framework.CategoryAttribute("navigate")]
-        public virtual void NavigateToTheEntryPage()
-        {
-            string[] tagsOfScenario = new string[] {
-                    "navigate"};
-            System.Collections.Specialized.OrderedDictionary argumentsOfScenario = new System.Collections.Specialized.OrderedDictionary();
-            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("Navigate to the entry page", null, tagsOfScenario, argumentsOfScenario, this._featureTags);
-#line 5
-this.ScenarioInitialize(scenarioInfo);
-#line hidden
-            bool isScenarioIgnored = default(bool);
-            bool isFeatureIgnored = default(bool);
-            if ((tagsOfScenario != null))
-            {
-                isScenarioIgnored = tagsOfScenario.Where(__entry => __entry != null).Where(__entry => String.Equals(__entry, "ignore", StringComparison.CurrentCultureIgnoreCase)).Any();
-            }
-            if ((this._featureTags != null))
-            {
-                isFeatureIgnored = this._featureTags.Where(__entry => __entry != null).Where(__entry => String.Equals(__entry, "ignore", StringComparison.CurrentCultureIgnoreCase)).Any();
-            }
-            if ((isScenarioIgnored || isFeatureIgnored))
-            {
-                testRunner.SkipScenario();
-            }
-            else
-            {
-                this.ScenarioStart();
-#line 6
- testRunner.Given("the user is signed in", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
-#line hidden
-#line 7
- testRunner.And("the user is on the homepage", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
-#line hidden
-#line 8
- testRunner.When("the covid entry link is clicked", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
-#line hidden
-#line 9
- testRunner.Then("the user is directed to the covid entry page", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
-#line hidden
-            }
-            this.ScenarioCleanup();
-        }
     }
 }
 #pragma warning restore
