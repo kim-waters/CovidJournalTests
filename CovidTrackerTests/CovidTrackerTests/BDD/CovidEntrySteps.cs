9e944ac6c59aa8ebdc0cf0562a65d92831124c0a
Kim Waters <kimwaters1996@gmail.com>
2021-03-17 11:41:26 +0000

    added signin page in lib

---
+++ CovidTrackerTests/CovidTrackerTests/BDD/CovidEntrySteps.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using CovidTrackerTests.lib.pages;
+
+namespace CovidTrackerTests.BDD
+{
+    [Binding]
+    public class CovidEntrySteps
+    {
+        public CJ_Website CJ_Website { get; } = new CJ_Website("chrome");
+
+        [Given(@"the user is signed in")]
+        public void GivenTheUserIsSignedIn()
+        {
+            CJ_Website.CJ_HomePage.Navigate();
+            CJ_Website.CJ_HomePage.ClickSignInLink();
+        }
+        
+        [Given(@"the user is on the homepage")]
+        public void GivenTheUserIsOnTheHomepage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"the covid entry link is clicked")]
+        public void WhenTheCovidEntryLinkIsClicked()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the user is directed to the covid entry page")]
+        public void ThenTheUserIsDirectedToTheCovidEntryPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
---
+++ CovidTrackerTests/CovidTrackerTests/lib/pages/CJ_LoginPage.cs
@@ -0,0 +1,44 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CovidTrackerTests.lib.pages
+{
+    public class CJ_LoginPage
+    {
+        private IWebDriver _driver;
+        private IWebElement _logInLink => _driver.FindElement(By.XPath("/html/body/div/main/div/div[1]/section/form/div[5]/button"));
+        private IWebElement _usernameField => _driver.FindElement(By.XPath("/html/body/div/main/div/div[1]/section/form/div[2]/input"));
+        private IWebElement _passwordField => _driver.FindElement(By.XPath("/html/body/div/main/div/div[1]/section/form/div[3]/input"));
+
+        public CJ_LoginPage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        public void Navigate()
+        {
+            _driver.Navigate().GoToUrl("https://localhost:44340/Identity/Account/Login");
+        }
+
+        public void ClickLogInLink()
+        {
+            _logInLink.Click();
+        }
+
+        public void EnterUsername(string username)
+        {
+            _usernameField.SendKeys(username);
+        }
+
+        public void EnterPassword(string password)
+        {
+            _usernameField.SendKeys(password);
+        }
+
+    }
+}
+
