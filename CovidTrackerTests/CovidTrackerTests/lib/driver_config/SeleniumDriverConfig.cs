a0d59011e18d042d1744c9a8cdc22f8e44e5e15d
Aemack <adam_mcgrane@msn.com>
2021-03-17 10:44:24 +0000

    Adding driver config and CJ_Website

---
+++ CovidTrackerTests/CovidTrackerTests/lib/driver_config/SeleniumDriverConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CovidTrackerTests.lib.driver_config
+{
+    class SeleniumDriverConfig
+    {
+    }
+}
